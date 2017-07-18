a225a07838ab9db3094eae551f58de814058f0ac
Frank Wiles <frank@revsys.com>
2017-07-18 16:41:47 -0500

    Code fixes and better README

--- README.md
+++ README.md
@@ -5,6 +5,44 @@ clients and staff.
 
 ## Installation
 
+To install you can simply run:
+
+    pip install https://github.com/revsys/rsdoc/
+
 ## Usage
 
+To create a new template for documentation simply run:
+
+    rsdoc create
+
+**NOTE**: To use the default REVSYS theme you will need to install [hugo](https://gohugo.io/) which for OSX is typically done with a simple `brew install hugo`
+
+To upload docs, simply finished editing your content in the directory you created (typically `./docs/`) and then run:
+
+    rsdoc upload ./docs/
+
+So the typically workflow becomes:
+
+1. Create docs skeleton using Cookiecutter via `rsdoc create`
+1. Edit your docs in `./docs/` using `hugo server` locally to verify everything is working correctly.
+1. cd into `./docs/` and run `hugo build`
+1. hugo will create a `public/` directory relative to your current path with the built files.
+1. To upload these simply run `rsdoc upload ./public/`
+
+Assuming your `.rsdoc` settings are correct this will tar up the contents of the hugo generated folder and deploy them via REST to docs.revsys.com
+
+## Configuration
+
+`rsdoc` uses an env style file named `.rsdoc` in the current directory.  Here is a simple template that includes all of the values you need:
+
+```
+RSDOC_PATH="<Your DocSet Path>"
+RSDOC_VERSION="<Your DocSet Version>"
+RSDOC_TOKEN="<Your DocSet Token>"
+```
+
+These will come from the DocSet model and associated upload Token defined in the system.
+
 ## Questions?
+
+Contact Frank
--- rsdoc.py
+++ rsdoc.py
@@ -1,5 +1,10 @@
 import click
+import glob
 import os
+import requests
+import subprocess
+import tarfile
+import tempfile
 import warnings
 
 from cookiecutter.main import cookiecutter
@@ -61,20 +66,61 @@ def upload(path, version, token, directory):
     # Can set these values
     path, version, token = handle_option_precendence(path, version, token)
 
-    print(path)
-    print(version)
-    print(token)
+    # Make tarfile in a temp file
+    tmpfile = tempfile.NamedTemporaryFile(delete=False)
+
+    with tarfile.open(fileobj=tmpfile, mode="w:gz") as tar:
+
+        old_cwd = os.getcwd()
+        os.chdir(directory)
+        for filename in glob.iglob('*'):
+            tar.add(filename, recursive=True)
+
+        os.chdir(old_cwd)
+
+    tmpfile.close()
+
+    # Upload it to the API
+    api_base = 'http://localhost/api/v1/upload'
+    filename = "{path}-{version}-{random}.tar.gz".format(
+        path=path,
+        version=version,
+        random=os.path.basename(tmpfile.name),
+    )
+
+    url = "{base}/{path}/{version}/{filename}/".format(
+        base=api_base,
+        path=path,
+        version=version,
+        filename=filename
+    )
+
+    fp = open(tmpfile.name, 'rb')
+
+    r = requests.post(
+        url=url,
+        files={'file': fp},
+        headers={'Authorization': 'Token {}'.format(token)}
+    )
+
+    fp.close()
+    os.unlink(tmpfile.name)
+
+    if r.status_code == 200:
+        click.secho("Docset updated", fg='green')
+    elif r.status_code == 201:
+        click.secho("New Docset version created", fg='green')
+    else:
+        click.secho("Upload error:", fg='red')
+        click.secho(r.json())
 
 
 @cli.command()
 @click.option('--cookiecutter-repo-url', default='https://github.com/revsys/revsys-doc-cookiecutter')
-@click.argument('directory', type=click.Path())
-def create(cookiecutter_repo_url, directory):
+def create(cookiecutter_repo_url):
     """ Create a new DocSet using the REVSYS cookiecutter template """
-    if os.path.exists(directory):
-        click.secho('ERROR: Directory {} already exists'.format(directory), fg='red')
-        raise click.Abort()
 
+    # Create the docs using cookiecutter
     click.secho('Running cookiecutter, it will ask you some questions:', fg='green')
     cookiecutter(cookiecutter_repo_url)
     click.secho('Cookiecutter creation finished!', fg='green')
